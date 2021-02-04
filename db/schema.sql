5923ba2d1372f6e94beea7bfe79aad6b8b427916
quaylas <quayla@cxadvisors.com>
2021-02-04 12:11:58 -0700

    updated with parties table

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,9 @@ first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 industry_connected BOOLEAN NOT NULL
 );
+
+CREATE TABLE parties (
+    id INTEGER PRIMARY KEY,
+    party_name VARCHAR(50) NOT NULL,
+    party_description text
+);
